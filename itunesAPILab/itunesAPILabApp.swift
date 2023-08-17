259c9c10ebdc65460953960d6ff8459d094b20b9
kauamiguel <kauamcm@gmail.com>
2023-08-17 17:23:13 -0300

    Salvando o projeto

---
+++ itunesAPILab/itunesAPILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesAPILabApp.swift
+//  itunesAPILab
+//
+//  Created by Kaua Miguel on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct itunesAPILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
